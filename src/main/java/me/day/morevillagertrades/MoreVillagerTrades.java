0a2dad255edda4da7fb727e01ce5ea4344b5aa54
Day <night-deploy@outlook.com>
2024-10-18 01:09:02 +0200

    Initial commit

---
+++ src/main/java/me/day/morevillagertrades/MoreVillagerTrades.java
@@ -0,0 +1,10 @@
+package me.day.morevillagertrades;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MoreVillagerTrades implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
